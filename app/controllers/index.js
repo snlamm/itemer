3164f7d52f8b77781cb1b0d9605b49137e08b706
snlamm <shmuel.lamm@gmail.com>
2016-06-29 16:09:35 -0400

    finish model setup for rails data

---
+++ app/controllers/index.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  gift: Ember.computed(function(){
+    return "hello"
+  }),
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    $.ajax({
+    return $.ajax({
       url: "http://localhost:3000/product_info",
       method: 'GET', dataType: 'json',
       data: {
@@ -10,12 +10,7 @@ export default Ember.Route.extend({
         keywords: "corderoy pants"
       },
       success: function(data){
-         let product_info = {
-           product_names: data.product_names,
-           prices: data.prices,
-           detail_page_urls: data.detail_page_urls
-         }
-         return product_info
+         return data
       },
       failure: function(){
         reject()
